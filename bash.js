91ac8b99af8b561f6115b23ed175b262d705df64
Henry Chong <chong.henry@gmail.com>
2019-11-11 12:45:31 -0500

    finished pwd

--- bash.js
+++ bash.js
@@ -1,1 +1,14 @@
-//test
+process.stdout.write('prompt >');
+// const exportsFromFileB = require('./fileB')
+
+process.stdin.on('data', (data) => {
+     
+    const cmd = data.toString().trim();
+    if (cmd === 'pwd') {
+        process.stdout.write(`${process.cwd(cmd)}`);
+    }
+ 
+
+    // process.stdout.write('\nprompt > ');
+});
+
